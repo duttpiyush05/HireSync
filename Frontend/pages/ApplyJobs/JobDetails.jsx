72fe59f6250d9b56d703950ed10c8ff65fdbdfeb
Piyush Dutt <bcs_2024048@iiitm.ac.in>
2026-07-10 18:05:07 +0530

    resolved-2

--- Frontend/pages/ApplyJobs/JobDetails.jsx
+++ Frontend/pages/ApplyJobs/JobDetails.jsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
-import  ClipLoader from 'react-spinners/ClipLoader'
 
 const JobDetails = () => {
   const [menuOpen, setMenuOpen] = useState(false)
